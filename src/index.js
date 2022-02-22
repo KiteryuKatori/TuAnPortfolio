4f6bc5cc44ded70aee7fdea64878bd3498eadb5a
KiteryuKatori <tsfhbc@gmail.com>
2022-02-22 18:45:43 +0700

    Adding componentns

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// import "../public/logo192.png"
+import "./style.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import Header from "./header.js"
+import MainContent from "./mainContent"
+import Footer from "./footer"
+
+
+function Page() {
+    return (
+        <div>
+            <Header/>
+            <MainContent/>
+            <Footer/>
+        </div>
+    )
+}
+
+ReactDOM.render(<Page/>, document.querySelector("#root"));
