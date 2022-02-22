0a319491015498f83d82b13f9887f8f86ac6b040
KiteryuKatori <tsfhbc@gmail.com>
2022-02-22 17:45:25 +0700

    Revert "imma delete u guys later, dont worry"
    This reverts commit d608fa11abfb7b55ad548936fe8549d9579815be.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// import "../public/logo192.png"
-import "./style.css"
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-import Header from "./header.js"
-import MainContent from "./mainContent"
-import Footer from "./footer"
-
-
-function Page() {
-    return (
-        <div>
-            <Header/>
-            <MainContent/>
-            <Footer/>
-        </div>
-    )
-}
-
-ReactDOM.render(<Page/>, document.querySelector("#root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
