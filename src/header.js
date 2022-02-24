4b579f3e590142fdb5821137ea6a25a62d2dce8a
KiteryuKatori <tsfhbc@gmail.com>
2022-02-24 17:43:38 +0700

    Fill Readme and index.html

--- src/header.js
+++ src/header.js
@@ -1,19 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import logo from "./imgs/logo192.png"
+// import logo from "./imgs/logo192.png"
 
 export default function Header() {
     return (
         <nav id="header">
             <div id="pageName">
-                <img src={logo}  alt="React's Logo"></img>
-                <h1>ReactFacts</h1>
+                {/* <img src={logo}  alt="React's Logo"></img> */}
+                <h1>HOME</h1>
             </div>
             <ul className="navLinksList">
-                <li className="navItem">Pricing</li>
-                <li className="navItem">Plans</li>
-                <li className="navItem">Others</li>
-                <li className="navItem">Contact</li>
+                <li className="navItem"><button>EN/US</button></li>
+                <li className="navItem"><button>VIE</button></li>
+                <li className="navItem"><button>LIGHT/DARK MODE</button></li>
             </ul>
         </nav>
     )
