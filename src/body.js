160699edd891f510950102bdc605b9625fbc1a5a
KiteryuKatori <tsfhbc@gmail.com>
2022-02-26 23:21:02 +0700

    fix imports in body.js and function name in SnE.js

--- src/body.js
+++ src/body.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ProfileSection from "./bodyComponents/profile";
+import SkillsandExperiencesSection from "./bodyComponents/SnE";
 
 export default function Body() {
     return (
         <div id="body">
             <ProfileSection />
+            <SkillsandExperiencesSection />
         </div>    
     )
 }
