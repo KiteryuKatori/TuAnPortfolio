a0f8425923235168c63ed527c882bf4874ca0410
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-02-28 15:21:09 +0700

    create more components and linking

--- src/components/SnE.js
+++ src/components/SnE.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {AiFillInfoCircle} from "react-icons/ai";
-import { IconContext } from "react-icons";
+
 
 export default function SkillsandExperiencesSection() {
     return (
