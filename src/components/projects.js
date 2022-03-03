52b4329c94987eae87009e475ffb5ae1dd8f5246
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-03-03 23:46:41 +0700

    fix PJ Section and fill

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import DBFLogo from "../imgs/PJs/DBFLogo.png";
+import Loto from "../imgs/PJs/Loto.png";
+import TTT from "../imgs/PJs/TTT.png";
+import BB from "../imgs/PJs/BB.png";
+import Playground from "../imgs/PJs/Playground.png";
+
 // import BasicInfo from "./extraComponents/profile/bsIn4";
 // import ExtraInfo from "./extraComponents/profile/extIn4";
+
+// const divStyle = {
+//     WebkitTransition: 'all', // note the capital 'W' here
+//     msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+//   };
+  
+//   function ComponentWithTransition() {
+//     return <div style={divStyle}>This should work cross-browser</div>;
+//   }
 
 export default function ProjectsSection() {
     return (
@@ -12,54 +28,64 @@ export default function ProjectsSection() {
 
             <ul className="sortBtnList">
                 <li className="sortBtn"><button>All</button></li>
-                <li className="sortBtn"><button>Ecommerce</button></li>
-                <li className="sortBtn"><button>Games</button></li>
-                <li className="sortBtn"><button>Others</button></li>
+                <li className="sortBtn notYet"><button>Games</button></li>
+                <li className="sortBtn notYet"><button>Others</button></li>
             </ul>
 
             <div className="pjContainer">
                 <div className="cell">
-                    <div className="cellHeader">
+                    <div style={{ backgroundColor: '#ffffff'}} className="cellHeader">
                         <div className="imgContainer">
-                            <img></img>
+                            <img style={{ transform: 'translateX(-50%), translateY(-50%)' }} src={DBFLogo} alt="Donation Box Foundation's Logo"></img>
                         </div>
                     </div>
                     <div className="cellContent">
-                        <div className="title">TicTacToe</div>
-                        <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sit.</div>
+                        <h2 className="title">Donation Box Foundation</h2>
+                        <p className="description">A non-profit charity Organization, created by a group of students to provide helps for the suffered people from the Covid-19 Pandemic.</p>
                     </div>
                 </div>
                 <div className="cell">
                     <div className="cellHeader">
                         <div className="imgContainer">
-                            <img></img>
+                            <img style={{ transform: 'translateX(-50%) translateY(-50%) scale(1.5)' }} src={Loto} alt="Loto(Mobile)'s gameplay"></img>
                         </div>
                     </div>
                     <div className="cellContent">
-                        <div className="title">TicTacToe</div>
-                        <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sit.</div>
+                        <h2 className="title">Loto</h2>
+                        <p className="description">One of the most iconic game in Viet Nam on Tet holidays. Its like Bingo, when you have to listen to a number and scan the paper to fill them in(if they exist on the paper) </p>
                     </div>
                 </div>
                 <div className="cell">
                     <div className="cellHeader">
                         <div className="imgContainer">
-                            <img></img>
+                            <img style={{ transform: 'translateX(-50%), translateY(-50%)' }} src={TTT} alt="Tic Tac Toe's gameplay"></img>
                         </div>
                     </div>
                     <div className="cellContent">
-                        <div className="title">TicTacToe</div>
-                        <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sit.</div>
+                        <h2 className="title">Tic Tac Toe</h2>
+                        <p className="description">A well-known game for it's name. Many have made replicas of this game and so do I.</p>
                     </div>
                 </div>
                 <div className="cell">
-                    <div className="cellHeader">
+                    <div  className="cellHeader">
                         <div className="imgContainer">
-                            <img></img>
+                            <img style={{ transform: 'translateX(-50%) translateY(-42.5%) scale(1.15)' }} src={BB} alt="Breaky Bricks's gameplay"></img>
                         </div>
                     </div>
                     <div className="cellContent">
-                        <div className="title">TicTacToe</div>
-                        <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sit.</div>
+                        <h2 className="title">Breaky Brick</h2>
+                        <p className="description">Another Brick Breaker genre</p>
+                    </div>
+                </div>
+                <div className="cell">
+                    <div  className="cellHeader">
+                        <div className="imgContainer">
+                            <img style={{ transform: 'translateX(-50%) translateY(-50%) scale(0.75)' }} src={Playground} alt="Interactive Playground"></img>
+                        </div>
+                    </div>
+                    <div className="cellContent">
+                        <h2 className="title">Interactive Playground</h2>
+                        <p className="description">A Gallery of buttons with animations that I have migrated other people's idea and implement them into mine.</p>
                     </div>
                 </div>
             </div>
--- src/header.js
+++ src/header.js
@@ -4,15 +4,15 @@ export default function Header() {
     return (
         <nav id="header">
             <ul className="sectionNav">
-                <li className="navItem"><a href="#About">About me</a></li>
+                <li className="navItem notYet"><a href="#About">About me</a></li>
                 {/* <li className="navItem"><a href="#SnE">Skills and Experiences</a></li> will add in the future */} 
-                <li className="navItem"><a href="#Projects">Projects</a></li>
-                <li className="navItem"><a href="#Contact">Contact</a></li>
+                <li className="navItem notYet"><a href="#Projects">Projects</a></li>
+                <li className="navItem notYet"><a href="#Contact">Contact</a></li>
             </ul>
             <ul className="utilitiesNav">
-                <li className="navItem"><button>EN/US</button></li>
-                <li className="navItem"><button>VIE</button></li>
-                <li className="navItem"><button>LIGHT/DARK MODE</button></li>
+                <li className="navItem notYet"><button>EN/US</button></li>
+                <li className="navItem notYet"><button>VIE</button></li>
+                <li className="navItem notYet"><button>LIGHT/DARK MODE</button></li>
             </ul>
         </nav>
     )
