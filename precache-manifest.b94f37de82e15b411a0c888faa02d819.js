b0cfe68ad77fd2673bca169ba624bee325c0c640
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-03-03 13:26:12 +0700

    Updates

---
+++ precache-manifest.b94f37de82e15b411a0c888faa02d819.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcdfedb74895140f459a",
+    "url": "/TuAnPortfolio/static/css/main.fcb7d3a5.chunk.css"
+  },
+  {
+    "revision": "bcdfedb74895140f459a",
+    "url": "/TuAnPortfolio/static/js/main.bcdfedb7.chunk.js"
+  },
+  {
+    "revision": "e8850c453158b51f0e3d",
+    "url": "/TuAnPortfolio/static/js/1.e8850c45.chunk.js"
+  },
+  {
+    "revision": "5a0faf0ae4b5988553d7",
+    "url": "/TuAnPortfolio/static/js/runtime~main.5a0faf0a.js"
+  },
+  {
+    "revision": "31a5938609f37527c6afa6845dccd5f2",
+    "url": "/TuAnPortfolio/index.html"
+  }
+];
